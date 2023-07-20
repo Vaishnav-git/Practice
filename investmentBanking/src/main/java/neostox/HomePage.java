aca283135a063d139adda1f8576aa3bcb3c9a0e5
vaish <vaish@Vaishnavi>
2023-07-20 17:12:04 +0530

    [Vaishnavi-git] new class created and remove from home page

--- investmentBanking/src/main/java/neostox/HomePage.java
+++ investmentBanking/src/main/java/neostox/HomePage.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-	
+	// home page
 	
 	@FindBy(xpath = "(//a[text()='Sign In'])[1]") private WebElement singInButton ;
 	
 	public HomePage(WebDriver driver) {
 		
 		PageFactory.initElements(driver, this);
+		
 	}
 	
 	public  void ClickOnSignIn() {
 		
 		singInButton.click();
-		System.out.println("Succesfully click on Sign In button");
 	}
-
+	
 }
---
+++ investmentBanking/src/main/java/neostox/Test2.java
@@ -0,0 +1,10 @@
+package neostox;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("change 1");
+	}
+
+}
